98f1463ec30f0d78477c33a1b00efd76b1cb15a9
Tejus <tejusvignesh@gmail.com>
2026-06-16 01:51:57 +0100

    Initial Commit

---
+++ LegitImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LegitImage
+//
+//  Created by Tejus Vijayakumar on 16/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LegitImage/LegitImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegitImageApp.swift
+//  LegitImage
+//
+//  Created by Tejus Vijayakumar on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct LegitImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
